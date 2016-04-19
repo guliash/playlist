7897f9f0eb6d094aa853d8dcddcc3bfbd2f5e49a
Artem Gilmudinov <guliash92@gmail.com>
2016-04-19 22:24:56 +0300

    description activity test - finishing test of displaying singer info;

--- app/src/test/java/com/github/guliash/playlist/ui/views/DescriptionActivityTest.java
+++ app/src/test/java/com/github/guliash/playlist/ui/views/DescriptionActivityTest.java
@@ -1,6 +1,7 @@
 package com.github.guliash.playlist.ui.views;
 
 import android.os.Build;
+import android.text.TextUtils;
 
 import com.github.guliash.playlist.BuildConfig;
 import com.github.guliash.playlist.structures.Singer;
@@ -49,6 +50,15 @@ public class DescriptionActivityTest {
     @Test
     public void singerDataCorrectlyShown() {
         activity.setSinger(singer);
-        assertTrue("Name view contains correct name", singer.name.equals(activity.name.getText()));
+        assertTrue("Name view contains correct text", TextUtils.equals(singer.name, activity.name
+                .getText()));
+        assertTrue("Tracks view contains correct text", TextUtils.equals(singer.tracks.toString(),
+                activity.tracks.getText()));
+        assertTrue("Albums view contains correct text", TextUtils.equals(singer.albums.toString(),
+                activity.albums.getText()));
+        assertTrue("Link view contains correct text", TextUtils.equals(singer.link,
+                activity.link.getText()));
+        assertTrue("Description view contains correct text", TextUtils.equals(singer.description,
+                activity.desc.getText()));
     }
 }
