72f0c36215a5738c1e790f37d767867177d670af
guliash <guliash92@gmail.com>
2016-07-16 18:28:31 +0300

    menu items for about and feedback;

---
+++ app/src/main/java/com/github/guliash/playlist/ui/views/FeedbackFragment.java
@@ -0,0 +1,21 @@
+package com.github.guliash.playlist.ui.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FeedbackFragment extends BaseFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
