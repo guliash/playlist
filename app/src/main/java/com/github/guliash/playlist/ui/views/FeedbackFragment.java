2e19d586acb692aefbe87ef48c8d2c943ec4c4b6
guliash <guliash92@gmail.com>
2016-07-17 13:54:29 +0300

    about and feedback functionalities;

--- app/src/main/java/com/github/guliash/playlist/ui/views/FeedbackFragment.java
+++ app/src/main/java/com/github/guliash/playlist/ui/views/FeedbackFragment.java
@@ -1,12 +1,34 @@
 package com.github.guliash.playlist.ui.views;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.github.guliash.playlist.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+//TODO There is a problem if toast shown. Fab will be overlapped
 public class FeedbackFragment extends BaseFragment {
+
+    @Bind(R.id.feedback)
+    EditText mFeedbackEdit;
+
+    @Bind(R.id.fab)
+    FloatingActionButton mSendFab;
+
+    private static final String EXAMPLE_MAIL = "guliash@example.com";
+    private static final String SUBJECT = "Playlist feedback";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,6 +38,28 @@ public class FeedbackFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_feedback, container, false);
+        ButterKnife.bind(this, view);
+
+        mSendFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail(mFeedbackEdit.getText().toString(), EXAMPLE_MAIL, SUBJECT);
+            }
+        });
+        return view;
+    }
+
+    private void sendMail(@NonNull String text, @NonNull String email, @NonNull String subject) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        try {
+            startActivity(Intent.createChooser(intent, "Send feedback..."));
+        } catch (ActivityNotFoundException e) {
+            Snackbar.make(mSendFab, R.string.no_mail_app_error, Snackbar.LENGTH_SHORT);
+        }
     }
 }
