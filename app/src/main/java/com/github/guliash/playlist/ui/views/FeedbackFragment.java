067b3a44e47a77df9e130dbae8d2b5e9d2212954
guliash <guliash92@gmail.com>
2016-07-19 20:06:37 +0300

    FeedbackFragment: added show to snack; butterknifed callbacks; removed empty methods;

--- app/src/main/java/com/github/guliash/playlist/ui/views/FeedbackFragment.java
+++ app/src/main/java/com/github/guliash/playlist/ui/views/FeedbackFragment.java
@@ -17,6 +17,7 @@ import com.github.guliash.playlist.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 //TODO There is a problem if toast shown. Fab will be overlapped
 //TODO make presenter
@@ -31,36 +32,26 @@ public class FeedbackFragment extends BaseFragment {
     @Bind(R.id.fab)
     FloatingActionButton mSendFab;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feedback, container, false);
         ButterKnife.bind(this, view);
-
-        mSendFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMail(mFeedbackEdit.getText().toString(), EXAMPLE_MAIL, SUBJECT);
-            }
-        });
         return view;
     }
 
-    private void sendMail(@NonNull String text, @NonNull String email, @NonNull String subject) {
+    @OnClick(R.id.fab)
+    void sendMail() {
+        String text = mFeedbackEdit.getText().toString();
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{EXAMPLE_MAIL});
+        intent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
         intent.putExtra(Intent.EXTRA_TEXT, text);
         try {
             startActivity(Intent.createChooser(intent, "Send feedback..."));
         } catch (ActivityNotFoundException e) {
-            Snackbar.make(mSendFab, R.string.no_mail_app_error, Snackbar.LENGTH_SHORT);
+            Snackbar.make(mSendFab, R.string.no_mail_app_error, Snackbar.LENGTH_SHORT).show();
         }
     }
 }
